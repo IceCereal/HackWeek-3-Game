19bc219e195ebcd39f3c37f45244273a078ddf55
IceCereal <ns_raghav@hotmail.com>
2019-08-17 17:57:32 +0530

    tiny dev updates

--- driver.py
+++ driver.py
@@ -13,7 +13,9 @@ parser = ArgumentParser()
 parser.add_argument('-i', "--imagePath", type = str, required = True, help = "path to the image.")
 parser.add_argument('-nc', "--numColors", type = int, required = True, help = "number of colors to be made.")
 parser.add_argument('-ps', "--pixelSize", type = int, required = True, help = "size of each pixel to be made.")
-parser.add_argument('-v', "--verbose", action="store_true", help="run driver and submodules with verbosity.")
+parser.add_argument('-v', "--verbose", action="store_true", help = "run driver and submodules with verbosity.")
+parser.add_argument('-dpxs', '--displayPixelSize', type = int, required = True, help = "size of each pixel to be shown: (dpxs * ps) will be actual pixel size shown.")
+parser.add_argument('-npx', '--numberPixels', type = int, help = "number of pixels to show per grid. (npx * npx) will be the number of pixels shown.")
 
 opt = parser.parse_args()
 
@@ -21,16 +23,16 @@ imageArray = Img2Pix(imagePath = opt.imagePath, K_Colors = opt.numColors, pixelS
 map = map(image = imageArray, pixelSize = opt.pixelSize, verbose = opt.verbose)
 
 pygame.init()
-window = pygame.display.set_mode((7*map.pixelSize*5, 7*map.pixelSize*5))
+window = pygame.display.set_mode((opt.displayPixelSize*map.pixelSize*opt.numberPixels, opt.displayPixelSize*map.pixelSize*opt.numberPixels))
 background_color = (255, 255, 255)
 window.fill(background_color)
 
 def disp(me_x, me_y):
-	a = [[0, 0, 0, 0, 0, 0, 0, 0] for i in range(8)]
-	for y in range(0, 8):
-		for x in range(0, 8):
-			yC = me_y - 3 + y
-			xC = me_x - 3 + x
+	a = [[0 for j in range(opt.numberPixels)] for i in range(opt.numberPixels)]
+	for y in range(0, opt.numberPixels):
+		for x in range(0, opt.numberPixels):
+			yC = me_y - int(opt.numberPixels/2) + y
+			xC = me_x - int(opt.numberPixels/2) + x
 
 			try:
 				a[y][x] = tuple(literal_eval(map.reference_colors_index[map.grid[xC][yC]['color']]))
@@ -41,11 +43,11 @@ def disp(me_x, me_y):
 				a[y][x] = (255, 255, 0)
 			if (xC == map.endPoint[1]) and (yC == map.endPoint[0]):
 				a[y][x] = (255, 255, 0)
-	a[3][3] = (255, 0, 0)
+	a[int(opt.numberPixels/2)][int(opt.numberPixels/2)] = (255, 0, 0)
 
-	for y in range(0, 8):
-		for x in range(0, 8):
-			rect = pygame.Rect(y*map.pixelSize*5, x*map.pixelSize*5, map.pixelSize*5, map.pixelSize*5)
+	for y in range(0, opt.numberPixels):
+		for x in range(0, opt.numberPixels):
+			rect = pygame.Rect(y*map.pixelSize*opt.numberPixels, x*map.pixelSize*opt.numberPixels, map.pixelSize*opt.numberPixels, map.pixelSize*opt.numberPixels)
 			pygame.draw.rect(window, a[y][x], rect)
 
 	return
--- map/map.py
+++ map/map.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 import operator
 from random import randint
 from time import sleep
+from ast import literal_eval
 
 class map:
 	def __init__(self, image : np.array, pixelSize : int, verbose : bool = None,):
@@ -17,24 +18,76 @@ class map:
 		self.reference_colors_index = []
 		self.startPoint = (0, 0)
 		self.endPoint = (0, 0)
+		self.key = (0, 0)
 
 		self.grid = []
 
 		if self.verbose:
 			print ("\nStart: Making Grid...")
-		self.__make_grid(image, pixelSize)
+		self.__make_grid(image)
 
 		self.__set_extra()
 
-	def __make_grid(self, image : np.array, pixelSize : int):
-		(width, height, depth) = image.shape
+	def __make_grid(self, image : np.array):
+		(height, width, depth) = image.shape
 
-		self.width = int(width / pixelSize)
-		self.height = int(height / pixelSize)
+		self.width = int(width / self.pixelSize)
+		self.height = int(height / self.pixelSize)
 
-		self.grid.append([{'color': -1, 'extra' : {'wall' : True}} for i in range(self.height+2)])
+		print (self.width, self.height)
+
+		for line in range(0, height, self.pixelSize):
+			for pixelUnit in range(0, width, self.pixelSize):
+				try:
+					color = str(image[line][pixelUnit])
+					if color[1] == ' ':
+						color = color.replace(' ', '', 1)
+					color = color.replace("  ", " ", 10)
+					color = color.replace(" ", ", ", 10)
+
+					color = literal_eval(color)
+					color = tuple(color)
+
+					if color not in self.reference_colors_index:
+						if self.verbose:
+							print ("Found New Color:\t", color)
+
+						self.reference_colors_index.append(color)
+						self.reference_colors[tuple(image[line][pixelUnit])] = 0
+
+					self.reference_colors[tuple(image[line][pixelUnit])] += 1
+				except IndexError:
+					pass
+
+		if (255, 255, 255) not in self.reference_colors_index:
+			if self.verbose:
+				print ("Adding (256, 256, 256)")
+
+			self.reference_colors_index.append((256, 256, 256))
+			self.reference_colors[(256, 256, 256)] = 
+		else:
+			print ("Manual Input Required! Exiting")
+			exit()
+
+		print (self.reference_colors)
+		print ("\n", self.reference_colors_index)
+
+		exit()
+
+		for line in range(0, height, self.pixelSize):
+			for pixelUnit in range(0, width, self.pixelSize):
+				color = str(image[line][pixelUnit]))
+				if color[1] == ' ':
+						color = color.replace(' ', '', 1)
+					color = color.replace("  ", " ", 10)
+					color = color.replace(" ", ", ", 10)
+
+					color = literal_eval(color)
+					color = tuple(color)
+
+		"""self.grid.append([{'color': -1, 'extra' : {'wall' : True}} for i in range(self.height+4)])
 		for line in range(0, width, pixelSize):
-			compressed_line = [{'color': -1, 'extra' : {'wall' : True}}]
+			compressed_line = [{'color': -1, 'extra' : {'wall' : True}}, {'color': -1, 'extra' : {'wall' : True}}]
 			for pixelUnit in range(0, height, pixelSize):
 				try:
 					color = str(image[line][pixelUnit])
@@ -60,8 +113,10 @@ class map:
 				except IndexError:
 					pass
 			compressed_line.append({'color': -1, 'extra' : {'wall' : True}})
+			compressed_line.append({'color': -1, 'extra' : {'wall' : True}})
+
 			self.grid.append(compressed_line)
-		self.grid.append([{'color': -1, 'extra' : {'wall' : True}} for i in range(self.height+2)])
+		self.grid.append([{'color': -1, 'extra' : {'wall' : True}} for i in range(self.height+4)])"""
 
 		if self.verbose:
 			print ("Complete: Making Grid!")
@@ -75,6 +130,8 @@ class map:
 				for pixel in line:
 					print (pixel['color'], end=' ')
 				print ("\n", end='')
+
+
 
 	def __set_extra(self):
 		if self.verbose:
